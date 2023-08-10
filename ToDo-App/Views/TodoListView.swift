111a767df40e5df7fc3333f185c9fafc90fc9325
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-08-10 13:29:39 -0400

    todo list view with firestoreQuerry updated

--- ToDo-App/Views/TodoListView.swift
+++ ToDo-App/Views/TodoListView.swift
@@ -6,18 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseFirestoreSwift
 
 struct TodoListView: View {
     @StateObject var viewModel = TodoListViewModel()
+    @FirestoreQuery var items: [TodoListItem]
     private let userId: String
     
     init(userId: String){
         self.userId = userId
+        //users/<id>/todos/<entries>
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
     }
     var body: some View {
         NavigationView{
             VStack{
-                
+                List(items) { item in
+                    Text(item.title)
+                }.listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar{
