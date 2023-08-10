ddc6d2cc5fae628cd4d74628190a7db96dc48866
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-08-09 21:41:20 -0400

    Initial commit

---
+++ ToDo-App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo-App
+//
+//  Created by Aerologix Aerologix on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RegisterLoginView()
+        }
+    }
+}
---
+++ ToDo-App/Views/RegisterLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  ToDo-App
+//
+//  Created by Aerologix Aerologix on 8/9/23.
+//
+
+import SwiftUI
+
+struct RegisterLoginView: View {
+    var body: some View {
+        NavigationView{
+            
+        }
+    }
+}
+
+#Preview {
+    RegisterLoginView()
+}
